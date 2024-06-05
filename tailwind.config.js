baf2be25975d176cab510f8f5b03f415478dfd8b
Muhammed Can Çetin <68066318+mcancetin@users.noreply.github.com>
2024-06-05 12:14:13 +0300

    theme customized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./location.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: "#d5966c",
+        "almost-black": "#151515",
+        "dark-grey": "#444444",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        sans: ['"Big Shoulders Display"', "sans-serif"],
+        secondary: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
